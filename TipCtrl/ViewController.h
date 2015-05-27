c55b9811f4b49b9fdd58f62fd0d54593a6f7f480
爱吃鱼的coder <hesong2ios@gmail.com>
2015-05-27 14:02:04 +0800

    Initial Commit

---
+++ TipCtrl/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TipCtrl
+//
+//  Created by hesong on 15/5/27.
+//  Copyright (c) 2015年 hesong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TipCtrl/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TipCtrl
+//
+//  Created by hesong on 15/5/27.
+//  Copyright (c) 2015年 hesong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
